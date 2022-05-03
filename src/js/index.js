10b0603d19cbdb5e641c673492b10468e6b02fed
Ruslan Sattarov <r8sattarov@mail.ru>
2022-05-03 13:10:37 +0500

    feat: add tabs

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,59 @@ linksLeft.forEach(linkLeft => {
     })
 })
 
+// ==================== tabs ==================== //
+
+const blockTopParent = document.querySelector('.block-top__tab');
+const everyday = document.querySelector('#everyday');
+const travel = document.querySelector('#travel');
+const running = document.querySelector('#running');
+const columns = document.querySelectorAll('.main-tabs__column')
+
+const addActiveBlockLink = (e) => {
+    const link = e.target;
+    console.log (link)
+    if (link.closest('#everyday')) {
+        everyday.classList.add('active-tab');
+        travel.classList.remove('active-tab');
+        running.classList.remove('active-tab');
+
+        columns.forEach(elem => {
+            if (elem.closest('.not-active-column')) {
+                elem.classList.remove('not-active-column');
+            }
+            elem.classList.add('active-column');
+        })
+
+
+    } else if (link.closest('#travel')) {
+        travel.classList.add('active-tab');
+        running.classList.remove('active-tab');
+        everyday.classList.remove('active-tab');
+
+        columns.forEach((elem, i) => {
+            if (elem.closest('.not-active-column')) {
+                elem.classList.remove('not-active-column');
+            }
+            if (i === 1) {
+                elem.classList.add('not-active-column');
+            }
+        })
+
+    } else if (link.closest('#running')){
+        running.classList.add('active-tab');
+        travel.classList.remove('active-tab');
+        everyday.classList.remove('active-tab');
+
+        columns.forEach((elem, i) => {
+            if (elem.closest('.not-active-column')) {
+                elem.classList.remove('not-active-column');
+            }
+            if (i === 2) {
+                elem.classList.add('not-active-column');
+            }
+        })
+    }
+}
+
+blockTopParent.addEventListener('click', addActiveBlockLink);
+
